f95d6458319b9814c0093d5e34c2e08f957a5f4c
MarcoConenna01 <marco.conenna01@gmail.com>
2023-07-02 11:11:40 +0200

    add ga

--- optimization.m
+++ optimization.m
@@ -10,17 +10,23 @@ clc
 objective = @(x) Objective_function(x,max(Thrust,[],"all"),max(Mass,[],"all"));
 
 % Define the initial guess
-x0 = [10, 0.8];
+x0 = [20, 0.12];
 
 % Define the nonlinear constraint function
 nonlcon = @(x) simp_constraints(x);
 
 % Define the optimization options
-options = optimoptions(@fmincon, 'Display', 'iter','Algorithm','sqp', 'FiniteDifferenceStepSize',0.001,'TolX', 1e-10, 'TolCon', 1e-3);
+options = optimoptions(@fmincon, 'Display', 'iter','Algorithm','interior-point', 'FiniteDifferenceStepSize',0.001,'TolX', 1e-10, 'TolCon', 1e-3);
 tic;
 % Run the optimization
 [x_opt, f_opt, exitflag, output,lambda,grad] = fmincon(objective, x0, [], [], [], [], [eps(1) Ln(1)], [eps(end) Ln(end)], nonlcon, options);
 toc;
+
+% options = optimoptions(@ga,'Display', 'iter');
+% tic
+% % Run the ga optimization
+% [x_opt, f_opt, exitflag, output,lambda,grad] = ga(objective, 2, [], [], [], [], [eps(1) Ln(1)], [eps(end) Ln(end)], nonlcon, options);
+% toc
 scatter(x_opt(1),x_opt(2),'*r')
 
 % Add a new element to the legend
